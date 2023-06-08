cfc0bd661ebf3f068be3e11300508695fb6033a2
andy-rust <mrruiner161@gmail.com>
2023-06-08 19:33:39 +0300

    добавил модель юзера и админку для нее

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,3 +1,70 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from .validators import USERNAME_VALIDATOR
+
+
+class User(AbstractUser):
+    '''Кастомная модель юзера
+    Поля email, username, first_name, last_name, password обязательны,
+    '''
+    USER = 'user'
+    ADMIN = 'admin'
+    GUEST = 'guest'
+    ROLE_CHOICES = [
+        (USER, 'user'),
+        (ADMIN, 'admin'),
+    ]
+
+    email = models.EmailField(
+        'email',
+        max_length=254,
+        unique=True,
+        blank=False
+    )
+    username = models.CharField(
+        'Логин',
+        max_length=150,
+        unique=True,
+        blank=False,
+        validators=[USERNAME_VALIDATOR]
+    )
+    first_name = models.CharField(
+        'Имя',
+        max_length=150,
+        blank=True
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150,
+        blank=True,
+    )
+    password = models.CharField(
+        'Пароль',
+        max_length=150,
+        blank=False
+    )
+    role = models.CharField(
+        'Роль пользователя',
+        max_length=50,
+        choices=ROLE_CHOICES,
+        default=USER,
+        blank=True
+    )
+
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN or self.is_superuser
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['username', 'email'],
+                name='unique_together',
+            )
+        ]
+
+    def __str__(self):
+        return self.username
